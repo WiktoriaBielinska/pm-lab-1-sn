767f470b634f9d453c41697ba608b67d19d94b18
Wiktoria <wiktoria.bielinska@student.wat.edu.pl>
2020-11-23 23:17:14 +0100

    Zadanie 2-2-1b

--- Projekt Zadanie-2-2/src/main.cpp
+++ Projekt Zadanie-2-2/src/main.cpp
@@ -1,15 +1,14 @@
 #include<Arduino.h>
 
+void setup(){
+  pinMode(LEB_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
 
-void setup() {
-		  // put your setup code here, to run once:
-		  pinMode(LED_BUILTIN, OUTPUT);
-		  digitalWrite(LED_BUILTIN, LOW);
-		}
-		
-
-		void loop() {
-		  // put your main code here, to run repeatedly:
-		  digitalWrite(LED_BUILTIN, HIGH);
-		  digitalWrite(LED_BUILTIN, LOW);
-		} 
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+} 
