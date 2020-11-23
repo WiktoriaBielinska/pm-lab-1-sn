ce1034fbe8878631e211aa2bad3d6a6e37dd38c9
Wiktoria <wiktoria.bielinska@student.wat.edu.pl>
2020-11-23 23:20:50 +0100

    Zadanie - 2-2

--- Projekt Zadanie-2-2/src/main.cpp
+++ Projekt Zadanie-2-2/src/main.cpp
@@ -1,15 +1,19 @@
 #include <Arduino.h>
+		
 
+		#define LED_RED 10
+		
 
-void setup(){
-  pinMode('LEB_BUILTIN', OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-} 
+		void setup() {
+		  // put your setup code here, to run once:
+		  pinMode(LED_BUILTIN, OUTPUT);
+		  digitalWrite(LED_BUILTIN, LOW);
+		}
+		
+		void loop() {
+		  // put your main code here, to run repeatedly:
+		  digitalWrite(LED_BUILTIN, HIGH);
+		  delay(500);
+		  digitalWrite(LED_BUILTIN, LOW);
+		  delay(500);
+		}
